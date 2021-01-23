fc48feb69a9b969cab338089b06ddbdfea2eacf2
sBeznosiuk <67642990+sBeznosiuk@users.noreply.github.com>
2021-01-23 03:50:43 +0200

    minor fixes

--- task-4.js
+++ task-4.js
@@ -3,17 +3,32 @@ let counterValue = 0;
 const decrementBtnRef = document.querySelector('button[data-action="decrement"]')
 const incrementBtnRef = document.querySelector('button[data-action="increment"]')
 const valueRef = document.querySelector('#value')
+valueRef.textContent = counterValue
 
 
-incrementBtnRef.addEventListener('click', () => {
+// incrementBtnRef.addEventListener('click', () => {
+//     counterValue += 1;
+//     console.log(counterValue)
+//     valueRef.textContent = counterValue
+// })
+
+// decrementBtnRef.addEventListener('click', () => {
+//     counterValue -= 1
+//     console.log(counterValue)
+//     valueRef.textContent = counterValue
+// })
+
+incrementBtnRef.addEventListener('click', handleIncrement)
+decrementBtnRef.addEventListener('click', handleDecrement)
+
+function handleIncrement () {
     counterValue += 1;
     console.log(counterValue)
     valueRef.textContent = counterValue
-})
+}
 
-decrementBtnRef.addEventListener('click', () => {
+function handleDecrement () {
     counterValue -= 1
     console.log(counterValue)
     valueRef.textContent = counterValue
-})
-
+}
--- task-7.js
+++ task-7.js
@@ -1,6 +1,10 @@
 const inputRef = document.querySelector('#font-size-control')
 const textRef = document.querySelector('#text')
 
+// inputRef.addEventListener('input', event => {
+//     textRef.style.fontSize = `${event.target.value}px`;
+// })
+
 inputRef.addEventListener('input', handleInputChange);
 
 function handleInputChange({ target }) {
