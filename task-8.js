778d606d761acbcf6acb409f425e3940f1a1efb7
sBeznosiuk <67642990+sBeznosiuk@users.noreply.github.com>
2021-01-27 04:09:35 +0200

    last-one

--- task-8.js
+++ task-8.js
@@ -6,26 +6,34 @@ const divRef = document.querySelector('#boxes')
 btnRender.addEventListener('click', () => createBoxes(inputRef.value))
 btnDestroy.addEventListener('click', destroyBoxes);
 
+let defWidth = 20;
+let defHeight = 20;
 
 function createBoxes(amount) {
 
-    for (let i = 0; i <= amount - 1; i++) {
-        const box = document.createElement('div')
-        divRef.appendChild(box)
-        console.log(box);
-        
-        const children = divRef.childNodes;
+    const divArray = [];
 
-        children.forEach((child, index) => {
-            child.style.width = `${index * 10 + 30}px`
-            child.style.height = `${index * 10 + 30}px`
-        })
+    for (let i = 0; i < amount; i++) {
+        const box = document.createElement('div')
+        console.log(box);
+
+        defWidth += 10;
+        defHeight += 10;
+        
+        box.style.width = `${defWidth}px`;
+        box.style.height = `${defHeight}px`;
           
         box.style.backgroundColor = `rgb(${Math.floor(Math.random() * (255 - 0) + 0)}, ${Math.floor(Math.random() * (255 - 0) + 0)}, ${Math.floor(Math.random() * (255 - 0) + 0)})`;
+
+        divArray.push(box)
     }
-    
+
+    divRef.append(...divArray)
 }
 
 function destroyBoxes() {
     divRef.innerHTML = '';
+
+    defHeight = 20;
+    defWidth = 20;
 }
